e88603d6e5a9e7c0acd71fcfe47425d821f447b8
tracer-ics2018 <tracer@njuics.org>
2018-12-13 08:36:38 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  08:36:38 up 25 min,  1 user,  load average: 0.21, 0.27, 0.44 1e4a7ab399955117e8aa59a1224907ce772d7c41

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -173,6 +173,7 @@ uint32_t eval(int p, int q) {
     return eval(p + 1, q - 1);
   } else {
     int op_pos = find_main_op(p, q);
+    printf("main op pos: %d\n", op_pos);
     uint32_t val_l = eval(p, op_pos - 1);
     uint32_t val_r = eval(op_pos + 1, q);
     if (val_l == EVAL_ERROR || val_r == EVAL_ERROR) return EVAL_ERROR;
