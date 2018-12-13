59eff500dfc0a838e7f34c8ba1926e7d5614607e
tracer-ics2018 <tracer@njuics.org>
2018-12-13 20:41:03 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:41:03 up  2:54,  1 user,  load average: 0.21, 0.30, 0.35 e5a3350b58d6d3fe42bc24187b7b030c643785e8

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -198,7 +198,7 @@ static int find_main_op(int p, int q) {
 uint32_t eval(int p, int q) {
   printf("%d, %d\n", p, q);
   if (p > q) {
-    printf("Bad expression.\n");
+    // printf("Bad expression.\n");
     return EVAL_ERROR;
   } else if (p == q) {
     assert(tokens[p].type == TK_DEC);
@@ -208,10 +208,13 @@ uint32_t eval(int p, int q) {
     return eval(p + 1, q - 1);
   } else {
     int op_pos = find_main_op(p, q);
-    printf("main op pos: %d\n", op_pos);
+    // printf("main op pos: %d\n", op_pos);
     uint32_t val_l = eval(p, op_pos - 1);
     uint32_t val_r = eval(op_pos + 1, q);
-    if (val_l == EVAL_ERROR || val_r == EVAL_ERROR) {printf("1\n"); return EVAL_ERROR;}
+    if (val_l == EVAL_ERROR || val_r == EVAL_ERROR) {
+      printf("main op pos: %d\n", op_pos);printf("1\n"); 
+      return EVAL_ERROR;
+    }
     switch(tokens[op_pos].type) {
       case '+': /*printf("%u\n", val_l + val_r);*/ return val_l + val_r;
       case '-': /*printf("%u\n", val_l - val_r);*/ return val_l - val_r;
