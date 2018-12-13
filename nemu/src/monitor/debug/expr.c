56fc33fbeaa532bf9ccb9c3da099fcf7c4dd4f7f
tracer-ics2018 <tracer@njuics.org>
2018-12-13 19:04:08 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:04:08 up  1:17,  1 user,  load average: 0.19, 0.17, 0.11 9f2052ba2748265aa4bb7bad188450c9b11b569b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -158,7 +158,7 @@ uint32_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   uint32_t res = eval(0, nr_token - 1);
   *success = res != EVAL_ERROR;
-  if (*success) printf("%u\n", res);
+  if (*success) return res;
   else printf("Failed\n");
   return 0;
 }
