b2a760e3fd52bf479d309881f667650510060128
U201514716-罗海旻 <hainesluo@gmail.com>
2018-12-13 20:54:39 +0800

    complete pa1.2

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -7,20 +7,20 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-  bool success = true;
-  uint32_t result;
-  char buf[65536];
-  while (scanf("%u %s", &result, buf) != EOF) {
-    bool equ = false;
-    uint32_t res = expr(buf, &success);
-    equ = res == result;
-    if (success) {
-      if (!equ) printf("%d %u %u %s\n", equ, res, result, buf);
-    } else {
-      printf("%u %s\n", result, buf);
-      printf("P Error\n");
-    }
-  }
+  // bool success = true;
+  // uint32_t result;
+  // char buf[65536];
+  // while (scanf("%u %s", &result, buf) != EOF) {
+  //   bool equ = false;
+  //   uint32_t res = expr(buf, &success);
+  //   equ = res == result;
+  //   if (success) {
+  //     if (!equ) printf("%d %u %u %s\n", equ, res, result, buf);
+  //   } else {
+  //     printf("%u %s\n", result, buf);
+  //     printf("P Error\n");
+  //   }
+  // }
 
   /* Receive commands from user. */
   
