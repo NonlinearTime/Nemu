bc469a9b35a1e204eaeb509e015ebb56dbef5d46
tracer-ics2018 <tracer@njuics.org>
2018-12-13 20:47:44 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:47:44 up  3:01,  1 user,  load average: 0.12, 0.16, 0.26 326c1d6fb3aef4681b2cac2114868a90fdad7979

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
     uint32_t res = expr(buf, &success);
     equ = res == result;
     if (success) {
-      if (!equ) ;//printf("%d %u %u %s\n", equ, res, result, buf);
+      if (!equ) printf("%d %u %u %s\n", equ, res, result, buf);
     } else {
       printf("%u %s\n", result, buf);
       printf("P Error\n");
