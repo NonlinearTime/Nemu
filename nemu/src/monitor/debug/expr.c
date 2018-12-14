5cac215c69a182892c473accef06d27bc015d441
tracer-ics2018 <tracer@njuics.org>
2018-12-14 11:12:36 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:12:36 up  3:06,  1 user,  load average: 0.32, 0.25, 0.25 5a1961a27e1f02c163ae6ec9a46de8a579eecfb

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -241,7 +241,7 @@ static int find_main_op(int p, int q) {
 }
 
 uint32_t eval(int p, int q) {
-  // printf("%d, %d\n", p, q);
+  printf("%d, %d\n", p, q);
   if (p > q) {
     printf("Bad expression.\n");
     return EVAL_ERROR;
@@ -282,7 +282,7 @@ uint32_t eval(int p, int q) {
     return eval(p + 1, q - 1);
   } else {
     int op_pos = find_main_op(p, q);
-    // printf("main op pos: %d\n", op_pos);
+    printf("main op pos: %d\n", op_pos);
     if (p == op_pos && (tokens[op_pos].type == TK_MINUS || tokens[op_pos].type == TK_DEREF)) {
       uint32_t val = eval (p + 1,q);
       switch (tokens[op_pos].type) {
