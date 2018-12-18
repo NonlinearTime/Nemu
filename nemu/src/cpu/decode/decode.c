a40160fd60b069b106f97a84c49fceb859b216e0
tracer-ics2018 <tracer@njuics.org>
2018-12-19 00:05:14 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  00:05:14 up  7:12,  1 user,  load average: 0.75, 0.78, 0.67 dab07b6dbed7542f2457c8f3ab7764a1277dbfcb

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -204,7 +204,7 @@ make_DHelper(r) {
 make_DHelper(jmp_call_I) {
   decode_op_SI(eip, id_dest, true);
   decoding.jmp_eip = *eip + id_dest->val;
-  printf("0x%x\n", id_dest->simm);
+  // printf("0x%x\n", id_dest->simm);
 }
 
 make_DHelper(E) {
