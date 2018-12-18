08ec6ee8c5a2d4f15634787904114a38dbbd2f87
tracer-ics2018 <tracer@njuics.org>
2018-12-19 00:29:52 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  00:29:52 up  7:37,  1 user,  load average: 0.45, 0.66, 0.68 f309d4291466ad85273ee1b97359f606cf86928

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -24,7 +24,7 @@ make_EHelper(and) {
 make_EHelper(xor) {
   interpret_rtl_xor(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
-
+  printf("%x\n", t0);
   print_asm_template2(xor);
 }
 
