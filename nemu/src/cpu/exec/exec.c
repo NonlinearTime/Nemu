50a70701075280c075f4122a2d2cc0a4b0ed916d
tracer-ics2018 <tracer@njuics.org>
2018-12-19 09:06:24 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:06:24 up 48 min,  1 user,  load average: 0.53, 0.28, 0.21 d7a8883fcdc384623ec5d831ed14e5e4244facc

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -249,7 +249,7 @@ void exec_wrapper(bool print_flag) {
 #endif
   // printf("eip: 0x%x\n", decoding.seq_eip);// rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
-  printf("eip: 0x%x, eax: 0x%x, ecx: 0x%x, edx: 0x%x, ebx: 0x%x, esp: 0x%x, ebp: 0x%x, esi: 0x%x, edi: 0x%X, eflags: 0x%x\n", 
+  printf("eip: 0x%x\neax: 0x%x, ecx: 0x%x, edx: 0x%x, ebx: 0x%x, esp: 0x%x, ebp: 0x%x, esi: 0x%x, edi: 0x%X, eflags: 0x%x\n", 
     ori_eip, reg_l(R_EAX), reg_l(R_ECX), reg_l(R_EDX), reg_l(R_EBX), reg_l(R_ESP), reg_l(R_EBP), reg_l(R_ESI), reg_l(R_EDI), cpu.eflags);
   update_eip();
 
