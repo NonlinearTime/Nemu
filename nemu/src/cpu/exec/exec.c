83712921bcb7507a196798ebb330fda239e04d08
tracer-ics2018 <tracer@njuics.org>
2018-12-19 08:48:33 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  08:48:33 up 30 min,  1 user,  load average: 0.06, 0.08, 0.23 8a0488a71d32a98c2e20a53065f41aade45ee021

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -227,6 +227,7 @@ static inline void update_eip(void) {
 
 void exec_wrapper(bool print_flag) {
   vaddr_t ori_eip = cpu.eip;
+  printf("ori_eip: 0x%x\n", ori_eip);
 
 #ifdef DEBUG
   decoding.p = decoding.asm_buf;
