674ee700e7fdd3d56f5f6152c207bbae3ce015e5
tracer-ics2018 <tracer@njuics.org>
2018-12-19 20:50:45 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:50:45 up  1:50,  1 user,  load average: 0.10, 0.52, 0.46 8ed22d41181e2896a1110c54f6006c1fd2f33ad6

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -239,19 +239,19 @@ void exec_wrapper(bool print_flag) {
   exec_real(&decoding.seq_eip);
 
 #ifdef DEBUG
-  // int instr_len = decoding.seq_eip - ori_eip;
-  // sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
-  // strcat(decoding.asm_buf, decoding.assembly);
-  // Log_write("%s\n", decoding.asm_buf);
-  // if (print_flag) {
-  //   puts(decoding.asm_buf);
-  // }
+  int instr_len = decoding.seq_eip - ori_eip;
+  sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
+  strcat(decoding.asm_buf, decoding.assembly);
+  Log_write("%s\n", decoding.asm_buf);
+  if (print_flag) {
+    puts(decoding.asm_buf);
+  }
 #endif
-  printf("\033[1;31m eip: 0x%x: %s, op: %x\n\033[0m", ori_eip, decoding.assembly, decoding.opcode);// rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-  // \033[1;31;40m eip: 0x%x\n \033[0m
-  printf(" eax: 0x%x, ecx: 0x%x, edx: 0x%x, ebx: 0x%x, esp: 0x%x, ebp: 0x%x, esi: 0x%x, edi: 0x%X, eflags: 0x%x\n", 
-    reg_l(R_EAX), reg_l(R_ECX), reg_l(R_EDX), reg_l(R_EBX), reg_l(R_ESP), reg_l(R_EBP), reg_l(R_ESI), reg_l(R_EDI), cpu.eflags);
-  printf(" cf:%d of:%d sf:%d zf:%d\n\n", cpu.CF, cpu.OF, cpu.SF, cpu.ZF);
+  // printf("\033[1;31m eip: 0x%x: %s, op: %x\n\033[0m", ori_eip, decoding.assembly, decoding.opcode);// rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  // // \033[1;31;40m eip: 0x%x\n \033[0m
+  // printf(" eax: 0x%x, ecx: 0x%x, edx: 0x%x, ebx: 0x%x, esp: 0x%x, ebp: 0x%x, esi: 0x%x, edi: 0x%X, eflags: 0x%x\n", 
+  //   reg_l(R_EAX), reg_l(R_ECX), reg_l(R_EDX), reg_l(R_EBX), reg_l(R_ESP), reg_l(R_EBP), reg_l(R_ESI), reg_l(R_EDI), cpu.eflags);
+  // printf(" cf:%d of:%d sf:%d zf:%d\n\n", cpu.CF, cpu.OF, cpu.SF, cpu.ZF);
   
   update_eip();
 
