0c9b7f8697e627ffd7ee25d7de166aeb4b55e81b
tracer-ics2018 <tracer@njuics.org>
2018-12-19 10:42:44 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:42:44 up  2:25,  1 user,  load average: 0.06, 0.13, 0.10 d7213a3190e3e9b0540b9aceb369f1dd6eb9be3

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -73,7 +73,7 @@ make_group(gp7,
 /* TODO: Add more instructions!!! */
 
 opcode_entry opcode_table [512] = {
-  /* 0x00 */	EMPTY, IDEX(G2E, add), EMPTY, EMPTY,
+  /* 0x00 */	EMPTY, IDEX(G2E, add), IDEX(E2G, add), EMPTY,
   /* 0x04 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x08 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
