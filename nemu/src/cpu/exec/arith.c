610349464da431031252c0ceb165858f6dcbae10
tracer-ics2018 <tracer@njuics.org>
2018-12-19 22:51:54 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:51:54 up  3:51,  1 user,  load average: 0.76, 0.64, 0.59 1451f6933f884823118fe8d73efa3b912b3eae94

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -41,6 +41,7 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
   rtl_sub(&t0, &id_dest->val, &id_src->val);
   // operand_write(id_dest, &t0);
+  printf("%x %x\n", id_dest->val, id_src->val);
 
   rtl_setrelop(RELOP_LTU, &t1, &id_dest->val, &t0);
   rtl_set_CF(&t1);
