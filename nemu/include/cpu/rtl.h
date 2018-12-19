4e70793fdda593074f257449b6da81a790a88c10
tracer-ics2018 <tracer@njuics.org>
2018-12-19 20:17:12 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:17:12 up  1:16,  1 user,  load average: 0.36, 0.36, 0.46 3fbcf664a49db6c9d962cfd13039417d99c01b1f

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -190,7 +190,7 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  *dest = (*src1) & ((1 << ((width << 3) - 1)) == 0) ? 0 : 1;
+  *dest = ((*src1) & (1 << ((width << 3) - 1))) == 0 ? 0 : 1;
 }
 
 #define make_rtl_setget_eflags(f) \
