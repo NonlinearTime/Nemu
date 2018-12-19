e6a22875407eab9664a51b0e6b2928e8e80895d5
tracer-ics2018 <tracer@njuics.org>
2018-12-19 09:02:18 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:02:18 up 44 min,  1 user,  load average: 0.28, 0.24, 0.20 e93c22a8e9772a98e23fa0b5a1a813bdb84a5507

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -239,13 +239,13 @@ void exec_wrapper(bool print_flag) {
   exec_real(&decoding.seq_eip);
 
 #ifdef DEBUG
-  int instr_len = decoding.seq_eip - ori_eip;
-  sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
-  strcat(decoding.asm_buf, decoding.assembly);
-  Log_write("%s\n", decoding.asm_buf);
-  if (print_flag) {
-    puts(decoding.asm_buf);
-  }
+  // int instr_len = decoding.seq_eip - ori_eip;
+  // sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
+  // strcat(decoding.asm_buf, decoding.assembly);
+  // Log_write("%s\n", decoding.asm_buf);
+  // if (print_flag) {
+  //   puts(decoding.asm_buf);
+  // }
 #endif
   printf("eip: 0x%x\n", decoding.seq_eip);// rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
