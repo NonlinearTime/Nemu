27954c653fd8d01da1f4ce2b30e80feeb0d39a25
tracer-ics2018 <tracer@njuics.org>
2018-12-20 21:19:16 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:19:16 up  3:12,  1 user,  load average: 1.38, 1.58, 1.48 acf4c5b6f5ce15d5890e008f61e151f3eb1726ef

--- nexus-am/tests/cputest/tests/string.c
+++ nexus-am/tests/cputest/tests/string.c
@@ -19,8 +19,8 @@ int main() {
 	//nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
 	//nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
 	
-	strcpy(str, str1);
-//	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
+	//strcpy(str, str1);
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
 	//nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
