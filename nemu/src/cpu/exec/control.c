4cdf26bd5ed2bdff67eb3e9807000442d79dad4f
tracer-ics2018 <tracer@njuics.org>
2018-12-20 10:24:30 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:24:30 up  2:12,  1 user,  load average: 0.11, 0.20, 0.21 a02891aa7fd3ca0219eb86e2c1d9f94242b2e09

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -45,10 +45,15 @@ make_EHelper(ret) {
 
 make_EHelper(call_rm) {
   printf("%x\n", *eip + id_dest->val);
-  if (decoding.is_operand_size_16)
-    rtl_j((*eip + id_dest->val) & 0x0000ffff);
-  else 
-    rtl_j(*eip + id_dest->val);
+  
+  if (decoding.is_operand_size_16) {
+    rtl_sext(&t0, &id_dest->val, 2);
+    rtl_j((*eip + t0) & 0x0000ffff);
+  }
+  else {
+    rtl_sext(&t0, &id_dest->val, 4);
+    rtl_j(*eip + t0);
+  }
 
   print_asm("call *%s", id_dest->str);
 }
