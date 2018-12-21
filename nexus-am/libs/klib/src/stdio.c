174a5337bf5fd3776bfb34a5577e859068321991
tracer-ics2018 <tracer@njuics.org>
2018-12-21 12:57:54 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:57:54 up  4:47,  1 user,  load average: 0.17, 0.23, 0.23 19b786f646d08f5af3fa02a52cbaf38887dba660

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,120 +4,85 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+  int i = 0;
+  char buffer[1024];
   va_list args;
-  int i,len;
-  char out[200];
-  //val = 0;
   va_start(args,fmt);
-  vsprintf(out,fmt,args);
+  vsprintf(buffer,fmt,args);
   va_end(args);
-  len = strlen(out);
-  for(i = 0;i < len;i ++)
-  {
-      _putc(out[i]);	  
-  }
+  while (buffer[i]) _putc(buffer[i++]);
+  
   return 0;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  char c;
-  char *str = out;
-  const char *tmp;
-  char num_s[100];
-  int i,j,len,num;
-  int flag,field_width;
-
-  for(;*fmt; fmt++)
-  {
-      if(*fmt != '%')
-      {
-	  *str++ = *fmt;
-	  continue;
-      }
-
-      flag = 0;
-      fmt++;
-      while(*fmt == ' ' || *fmt == '0')
-      {
-	if(*fmt == ' ')  flag |= 8;
-	else if(*fmt == '0') flag |= 1;
-	fmt++;
-      }
-      
-      field_width = 0;
-      if(*fmt >= '0' && *fmt <= '9')
-      {
-	      while(*fmt >= '0' && *fmt <= '9')
-	      {
-		      field_width = field_width*10 + *fmt -'0';
-		      fmt++;
-	      }
-      }
-      else if(*fmt == '*')
-      {
-	      fmt++;
-	      field_width = va_arg(ap,int);
-      }
-      //base = 10;
-
-      switch(*fmt)
-      {
-	  case 's':
-	      tmp = va_arg(ap,char *);
-	      len = strlen(tmp);
-	      for(i = 0;i < len;i ++)
-	      {
-		   *str++ = *tmp++;
-	      }
-	      continue;
-	  case 'd': break;
-      }
-
-      num = va_arg(ap,int);
-      j = 0;
-      if(num == 0)
-      {
-	  num_s[j++] = '0';
-      }
-      else
-      {
-	  if(num < 0)
-	  {
-	      *str++ = '-';
-	      num = -num;
-	  }
-	  //j = 0;
-	  while(num)
-	  {
-	      num_s[j++] = num%10 + '0';
-	      num /= 10;
-	  }
-      }
-      if(j < field_width)
-      {
-	      num = field_width - j;
-	      c = flag & 1 ? '0' : ' ';
-	      while(num--)
-	      {
-		      *str++ = c;
-	      }
-      }
-      while(j--)
-      {
-	  *str++ = num_s[j];
-      }
+int write_Int(char* buffer,int value) {
+  int len = 0;
+  char rec[100];
+  int vt = value;
+  if (value < 0) {
+    value *= -1;
+    *(buffer++)='-';
   }
-  *str = '\0';
+  while (1) {
+    int t = value % 10;
+    rec[len++] = '0' + t;
+    value /= 10;
+    if (!value) break;
+  }
+
+  int tmp = len;
+  while (tmp--) {
+    *buffer++ = rec[tmp - 1];
+  }
+
+  return vt >= 0 ? len : len + 1;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  char* buffer = out;
+  // unsigned int buffer_length=strlen(buffer);
+  unsigned int fmt_length=strlen(fmt);
+  unsigned int index=0;
+     
+  int temp;
+  char ctemp;
+  char* stemp;
+     
+  for(index=0;index<fmt_length;++index)
+  {
+    if(fmt[index]!='%')
+      (*buffer++)=fmt[index];
+    else {
+      index++;
+      switch(fmt[index]) {
+        case 'd':
+          temp=va_arg(ap,int);
+          buffer=buffer+write_Int(buffer,temp);
+          break;
+        case 's':
+          stemp=(char*)va_arg(ap,char*);
+          strcpy(buffer,stemp);
+          buffer+=strlen(stemp);
+          break;
+        case 'c':
+          ctemp=va_arg(ap,int);
+          *(buffer++)=ctemp;
+          break;
+      }
+    }
+  }
+  *buffer = '\0';
+
   return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list args;
-  int  val;
+  int ret;
   va_start(args,fmt);
-  val = vsprintf(out,fmt,args);
+  ret = vsprintf(out,fmt,args);
   va_end(args);
-  return val;
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
