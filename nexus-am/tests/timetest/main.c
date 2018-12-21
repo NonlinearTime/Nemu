2094e9fd5fdec8b175c9d90a0f885b872aa4302f
tracer-ics2018 <tracer@njuics.org>
2018-12-21 12:50:51 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:50:51 up  4:40,  1 user,  load average: 0.24, 0.17, 0.23 47a9641d304f3d83c09017f532175963e1eeb97

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -3,19 +3,20 @@
 #include <klib.h>
 
 int main(){
-  _RTCReg rtc;
+  // _RTCReg rtc;
   _ioe_init();
   int sec = 1;
-  while (sec < 3) {
-    while(uptime() < 1000 * sec) ;
-    get_timeofday(&rtc);
-    printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
-    if (sec == 1) {
-      printf("%d second).\n", sec);
-    } else {
-      printf("%d seconds).\n", sec);
-    }
-    sec ++;
-  }
+  printf("%d\n", sec);
+  // while (sec < 3) {
+  //   while(uptime() < 1000 * sec) ;
+  //   get_timeofday(&rtc);
+  //   printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+  //   if (sec == 1) {
+  //     printf("%d second).\n", sec);
+  //   } else {
+  //     printf("%d seconds).\n", sec);
+  //   }
+  //   sec ++;
+  // }
   return 0;
 }
