44c761ecaf0c2bf6e3428d2b4cfbd2c544ff085d
tracer-ics2018 <tracer@njuics.org>
2018-12-21 13:27:15 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:27:15 up  5:16,  1 user,  load average: 1.06, 0.41, 0.24 915602eee39003f885bc53c41d6f7779020bbd

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -10,7 +10,7 @@ int main(){
   while (1) {
     while(uptime() < 1000000 * sec) ;
     get_timeofday(&rtc);
-    printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+    // printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
       printf("%d second).\n", sec);
     } else {
