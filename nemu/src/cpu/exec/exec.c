cfff0c132ebf788c7a911e3b79d8a5e3eb1763f2
tracer-ics2018 <tracer@njuics.org>
2018-12-21 14:17:12 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:17:12 up  6:06,  1 user,  load average: 0.20, 0.27, 0.22 76d48e63e6f8672bb4eb47067628f57ad98c3da0

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -43,7 +43,7 @@ static make_EHelper(name) { \
 // logic ?
 make_group(gp1,
     EX(add), EX(or), EX(adc), EX(sbb),
-    EX(and), EX(sub), EMPTY, EX(cmp))
+    EX(and), EX(sub), EX(xor), EX(cmp))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
