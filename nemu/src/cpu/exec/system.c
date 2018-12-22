03448e42f554e10089bdafca0ea9d353122a6b95
tracer-ics2018 <tracer@njuics.org>
2018-12-22 19:39:15 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:39:15 up  5:55,  1 user,  load average: 0.43, 0.31, 0.39 68d8afa6f80cb609751f539527aca0bb3e7c6b37

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -8,12 +8,13 @@ make_EHelper(lidt) {
   // interpret_rtl_host_lm(&cpu.idtr.limit, id_dest->val, 2);
   // interpret_rtl_host_lm(&cpu.idtr.base, id_dest->val + 16, 4);
   cpu.idtr.limit = vaddr_read(id_dest->val, 2);
+  cpu.idtr.base = vaddr_read(id_dest->val + 16, 4);
 #ifdef DEBUG
   printf("lidt: 0x%x\n", id_dest->val);
   printf("lidt: 0x%x\n", id_dest->val + 16);
 #endif
   printf("lidt: 0x%x 0x%x\n", id_dest->val, cpu.idtr.base);
-  cpu.idtr.base = vaddr_read(id_dest->val + 16, 4);
+  
   if (decoding.is_operand_size_16) {
     cpu.idtr.base &= 0x00ffffff;
   }
