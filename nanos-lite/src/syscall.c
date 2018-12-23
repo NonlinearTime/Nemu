db16218a46637a80a6a936a09eea83783ac4a5fa
tracer-ics2018 <tracer@njuics.org>
2018-12-23 16:51:55 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:51:55 up  6:46,  1 user,  load average: 0.22, 0.26, 0.27 c55845c622609645e91f7a0adebc25ee76278fa

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,7 @@ uintptr_t sys_yield();
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("%d\n", c->GPR1);
+  // printf("%d\n", c->GPR1);
 
   switch (a[0]) {
     case SYS_yield: c->GPR1 = sys_yield(); break;
