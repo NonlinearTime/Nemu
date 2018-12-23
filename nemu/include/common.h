854c532fb1c9ba08bc0c25f1290acfa7da6ab441
tracer-ics2018 <tracer@njuics.org>
2018-12-23 20:10:44 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:10:44 up 10:05,  1 user,  load average: 1.21, 0.73, 0.90 8549a94f8a0bb6aeb3a20a36a5d7efb7f904278

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -1,8 +1,8 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
-#define DEBUG
-#define DIFF_    TEST
+// #define DEBUG
+// #define DIFF_    TEST
 
 #if _SHARE
 // do not enable these features while building a reference design
