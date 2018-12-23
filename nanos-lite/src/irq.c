070b464e6f9c355968421de552cf8d8654fe22f8
tracer-ics2018 <tracer@njuics.org>
2018-12-23 12:20:01 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:20:01 up  2:14,  1 user,  load average: 1.30, 0.78, 0.70 8617af456c6509177cfe95047de89b508ced03eb

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: panic("event ID = %d", e.event); break;
+    case _EVENT_YIELD: printf("event ID = %d", e.event); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
