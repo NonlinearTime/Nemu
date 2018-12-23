3d26b01504b621f7673effdb3d553a87c105020d
tracer-ics2018 <tracer@njuics.org>
2018-12-23 10:48:04 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:48:04 up 42 min,  1 user,  load average: 0.83, 0.63, 0.58 4a8de82084bc9964daeb41441b4cd4b373bd94ae

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -7,11 +7,22 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 void vectrap();
 void vecnull();
 
+// struct _Context {
+//   uintptr_t esp;
+//   struct _Protect *prot;
+//   uintptr_t edi, esi, ebp, ori_esp, ebx, edx, ecx, eax;
+//   int       irq;
+//   uintptr_t err, eip, cs, eflags;
+
+// };
+
 _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
   printf("irq_handle:\n");
-  printf("eax: 0x%x, ecx: 0x%x, edx: 0x%x, ebx: 0x%x, esp: 0x%x, ebp: 0x%x, esi: 0x%x, edi: 0x%x, eflags: 0x%x\n", 
-  tf->eax, tf->ecx, tf->edx, tf->ebx, tf->esp, tf->ebp, tf->esi, tf->edi, tf->eflags);
+  // printf("eax: 0x%x, ecx: 0x%x, edx: 0x%x, ebx: 0x%x, esp: 0x%x, ebp: 0x%x, esi: 0x%x, edi: 0x%x, eflags: 0x%x\n", 
+  // tf->eax, tf->ecx, tf->edx, tf->ebx, tf->esp, tf->ebp, tf->esi, tf->edi, tf->eflags);
+  printf("esp: 0x%x, prot: 0x%x, esi: 0x%x, ebp: 0x%x, ori_esp: 0x%x, ebx: 0x%x, edx: 0x%x, ecx: 0x%x, eax: 0x%x, irq: 0x%x, err: 0x%x, eip: 0x%x, cs: 0x%x, eflags: 0x%x\n", 
+  tf->esp, tf->prot, tf->esi, tf->ebp, tf->ori_esp, tf->ebx, tf->edx, tf->ecx, tf->eax, tf->irq, tf->err, tf->eip, tf->cs, tf->eflags);;
   return tf;
   if (user_handler) {
     _Event ev = {0};
