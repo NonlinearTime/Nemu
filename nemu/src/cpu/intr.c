b2493fd76e57e332e0c1e6a47944071801b22fdd
tracer-ics2018 <tracer@njuics.org>
2018-12-23 16:46:25 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:46:25 up  6:40,  1 user,  load average: 0.44, 0.28, 0.27 8ca319a9690582cfa1a1f7608c08140162360

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,6 +9,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eflags);
   rtl_push(&cpu.cs);
   rtl_push(&ret_addr);
+  NO = NO - 1;
   t0 = cpu.idtr.base;
   t1 = t0 + 8 * NO;
   printf("raise_intr: 0x%x 0x%x 0x%x\n",NO, t0, t1);
