c8f74af81267ff8348f34a1b13baaac5a4264e95
tracer-ics2018 <tracer@njuics.org>
2018-12-23 20:00:13 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:00:13 up  9:54,  1 user,  load average: 0.85, 1.04, 1.16 4c396069fbbb2886c9b0081cf181f63c16863bf5

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,16 +3,14 @@
 
 int main() {
   write(1, "Hello World!\n", 13);
-  write(1, "Hello World!\n", 13);
   int i = 2;
-  printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  // volatile int j = 0;
-  // while (1) {
-  //   j ++;
-  //   if (j == 10000) {
-  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  //     j = 0;
-  //   }
-  // }
+  volatile int j = 0;
+  while (1) {
+    j ++;
+    if (j == 10000) {
+      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      j = 0;
+    }
+  }
   return 0;
 }
