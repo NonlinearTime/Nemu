9b797d9388ff9d268ff245acde35a4823c67b34f
tracer-ics2018 <tracer@njuics.org>
2018-12-24 23:36:52 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:36:52 up 12 min,  1 user,  load average: 0.43, 0.78, 0.60 7a9354398b40e8653a78872acef55063be474a0d

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -8,9 +8,9 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
-      uint32_t nt = inw(TIMER_PORT);
-      uptime->hi = nt >> 16;
-      uptime->lo = nt & 0xffff;
+      uint64_t nt = inl(TIMER_PORT);
+      uptime->hi = nt >> 32;
+      uptime->lo = nt & 0xffffffff;
       // printf("lo: %x\n", uptime->lo);
       return sizeof(_UptimeReg);
     }
