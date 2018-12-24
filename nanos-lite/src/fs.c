818f07f96d3e28d066d255b4d9307de4d1c1cd65
tracer-ics2018 <tracer@njuics.org>
2018-12-24 21:37:25 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:37:25 up 13:34,  1 user,  load average: 0.52, 0.87, 1.12 f115decdca0b7dbec587d5b1550d54314c2f91a2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ int fs_open(const char *pathname, int flags, int mode) {
   for (i = 0; i < NR_FILES; ++i) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
-      // Log("fs_open: %s\n",pathname);
+      Log("fs_open: %s\n",pathname);
       return i;
     }
   }
@@ -70,7 +70,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
   size_t l = file_table[fd].open_offset + len <= file_table[fd].size ? len : file_table[fd].size - file_table[fd].open_offset;
   // ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
-  // Log("fs_read: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
+  Log("fs_read: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
   return l; 
 }
