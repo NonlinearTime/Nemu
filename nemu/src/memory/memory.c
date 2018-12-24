bf14a0e90eaeb671e333f5752e4005fdf727d522
tracer-ics2018 <tracer@njuics.org>
2018-12-24 20:24:38 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:24:38 up 12:22,  1 user,  load average: 0.53, 0.31, 0.31 81cfde5ffc4d0ed3acd3c7ffaedf22372796a602

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,7 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
+  printf("addr: %d\n", addr);
   int p = is_mmio(addr);
   if (p == -1)
     return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
