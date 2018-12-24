b59b7dbff46303c9ad4602b2c4c303be68dbd2fd
tracer-ics2018 <tracer@njuics.org>
2018-12-24 19:05:35 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:05:35 up 11:03,  1 user,  load average: 0.42, 0.22, 0.11 e160656457242b95af76d16c4b97f17760167fe1

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -36,7 +36,7 @@ int _write(int fd, void *buf, size_t count){
   // return 0;
 }
 
-intptr_t program_brk_old;
+static intptr_t program_brk_old;
 
 void *_sbrk(intptr_t increment){
   // printf("SRK: %p\n",program_brk);
