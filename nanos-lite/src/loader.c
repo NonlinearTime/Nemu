6c7dc762afabe02452893102a69d4a0ffb8bb1f1
tracer-ics2018 <tracer@njuics.org>
2018-12-24 23:49:23 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:49:23 up 25 min,  1 user,  load average: 0.53, 0.58, 0.63 b2d3b09e84989b2574548df1c2a5004420f6d78c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,7 +16,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   uint32_t len = fs_filesz(fd);
   uint32_t blen = 1024;
-  uint32_t s = DEFAULT_ENTRY;
+  uintptr_t s = DEFAULT_ENTRY;
   // // ramdisk_read(buf, 0, len);
   char buf[blen];
   while (len > blen) {
