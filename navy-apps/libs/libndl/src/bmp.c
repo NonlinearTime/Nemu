54b235fefbf1c62f42e9e53a1c8c97377a0c47fe
tracer-ics2018 <tracer@njuics.org>
2018-12-24 19:51:00 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:51:00 up 11:48,  1 user,  load average: 0.40, 0.39, 0.30 3b2daf79c57ae2b9991df0fdf915fc4e4b21826c

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -40,7 +40,7 @@ int NDL_LoadBitmap(NDL_Bitmap *bmp, const char *filename) {
   w = hdr.width; h = hdr.height;
   int line_off = (w * 3 + 3) & ~0x3;
   printf("line_off %d, w %d, h %d", line_off, w, h);
-  assert(0);
+  // assert(0);
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
     int nread = fread(&pixels[w * i], 3, w, fp);
