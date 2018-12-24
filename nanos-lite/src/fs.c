7e4cc9240f45e754f3a24c33f2a7d0e03048d683
tracer-ics2018 <tracer@njuics.org>
2018-12-24 20:33:16 +0800

    >  compile U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:33:16 up 12:30,  1 user,  load average: 0.33, 0.44, 0.44 6ddda8b4d95f265360c555ff84848bf8581d01f7

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ int fs_open(const char *pathname, int flags, int mode) {
   for (i = 0; i < NR_FILES; ++i) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
-      Log("fs_open: %s\n",pathname);
+      // Log("fs_open: %s\n",pathname);
       return i;
     }
   }
@@ -70,7 +70,7 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
   size_t l = file_table[fd].open_offset + len <= file_table[fd].size ? len : file_table[fd].size - file_table[fd].open_offset;
   // ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
-  Log("fs_read: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
+  // Log("fs_read: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
   return l; 
 }
@@ -81,12 +81,12 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
   // ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
-  Log("fs_write: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
+  // Log("fs_write: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
   return l;
 }
 
 off_t fs_lseek(int fd, off_t offset, int whence) {
-  Log("fs_lseek: %s open_offset: 0x%x offset: 0x%x\n whence: %d\n", file_table[fd].name, file_table[fd].open_offset, offset, whence);
+  // Log("fs_lseek: %s open_offset: 0x%x offset: 0x%x\n whence: %d\n", file_table[fd].name, file_table[fd].open_offset, offset, whence);
   switch(whence) {
     case SEEK_SET: assert(offset <= file_table[fd].size); file_table[fd].open_offset = offset; break;
     case SEEK_CUR: assert(file_table[fd].open_offset + offset <= file_table[fd].size); file_table[fd].open_offset += offset; break;
