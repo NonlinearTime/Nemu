f4f11cf7316d7b0dbb1135f1e768acdaf27fe90d
tracer-ics2018 <tracer@njuics.org>
2018-12-24 09:43:35 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:43:35 up  1:41,  1 user,  load average: 0.48, 0.31, 0.24 5c87185848b7b64196f4ac2e7b2ef2044468502e

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -51,7 +51,7 @@ int _read(int fd, void *buf, size_t count) {
 }
 
 int _close(int fd) {
-  return _syscall_(SYS_close, fd);
+  return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
