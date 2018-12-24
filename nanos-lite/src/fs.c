ef62aee2a6bf03ff58b2d9e7829c6b7838721f44
tracer-ics2018 <tracer@njuics.org>
2018-12-24 13:47:07 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:47:07 up  5:44,  1 user,  load average: 0.27, 0.24, 0.27 eaec13c847c93e0a98376607fdaa395f1c09f5c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,7 +80,7 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
   // ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
-  Log("fs_write: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
+  // Log("fs_write: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
   return l;
 }
 
