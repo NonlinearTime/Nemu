b2439e2ef8b8b84f215b29bf9fc787d9ffac9404
tracer-ics2018 <tracer@njuics.org>
2018-12-24 13:46:16 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:46:16 up  5:43,  1 user,  load average: 0.22, 0.23, 0.27 8d7ba4292b6894deb853b9c31742a42e8a343951

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -38,10 +38,10 @@ int _write(int fd, void *buf, size_t count){
 
 void *_sbrk(intptr_t increment){
   // printf("SRK: %p\n",program_brk);
-  if (program_brk == 0) program_brk = end;
+  if (program_brk == 0) program_brk = c;
   char buf[30];
   buf[29] = '\0';
-  sprintf(buf, "SRK: %p\n",program_brk);
+  sprintf(buf, "SRK: %p\n",(void *)(int)end);
   _write(1, buf, 9);
   void* program_brk_old = program_brk;
   if (_syscall_(SYS_brk, program_brk + increment, 0, 0) == 0) 
