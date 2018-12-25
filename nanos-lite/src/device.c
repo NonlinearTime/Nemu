970d82bdd043a570a2d81f2c319e9e9457d256aa
tracer-ics2018 <tracer@njuics.org>
2018-12-25 10:28:22 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:28:22 up  1:41,  1 user,  load average: 1.63, 1.56, 1.08 830459e82a9a4ca838f06fbda9ee3f6f17ad9bfc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -33,7 +33,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   int x = (offset / 4) / screen_width();
   int y = (offset / 4) % screen_height();
-  Log("fb_write: x %d y %d\n", x, y);
+  Log("fb_write: x %d y %d len %d\n", x, y, len);
   draw_rect((uint32_t* )buf, 0, 0, x, y);
   return len;
 }
