c380bed9018b1389c51d122578370ee626ed7a26
tracer-ics2018 <tracer@njuics.org>
2018-12-25 23:47:49 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:47:49 up 11:42,  1 user,  load average: 1.51, 1.47, 1.12 544b0590c799df6a6d3a5874c84c10e92685b9db

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -74,10 +74,10 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
   // assert(file_table[fd].open_offset + len <= file_table[fd].size);
   size_t l = file_table[fd].open_offset + len <= file_table[fd].size ? len : file_table[fd].size - file_table[fd].open_offset;
   if (file_table[fd].read == NULL) ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
-  else file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
+  else l = file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   // Log("fs_read: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
-  return len; 
+  return l; 
 }
 
 ssize_t fs_write(int fd, const void *buf, size_t len) {
