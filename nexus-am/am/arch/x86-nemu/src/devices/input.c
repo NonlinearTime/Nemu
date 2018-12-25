63f109b521685bc538b42c164930c5aa5df07989
tracer-ics2018 <tracer@njuics.org>
2018-12-25 19:49:20 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:49:20 up  7:43,  1 user,  load average: 0.21, 0.47, 0.57 17921e3474d2c41ed49adbf86df35bef66eba67

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -8,7 +8,7 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
       uint32_t p = inl(KEYBORD_PORT);
-      kbd->keydown = p == _KEY_NONE ? kbd->keydown : !(kbd->keydown);
+      kbd->keydown = ((p & 0x8000) == 0) ? 0: 1;
       kbd->keycode = p;
       return sizeof(_KbdReg);
     }
