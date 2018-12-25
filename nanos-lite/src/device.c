55db125ba5437bdbfe48bfd457e7a2db496bff11
tracer-ics2018 <tracer@njuics.org>
2018-12-25 20:17:18 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:17:18 up  8:11,  1 user,  load average: 0.66, 0.73, 0.82 40d854fc7055a85a480f989ca01142bcada85c4

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -45,6 +45,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   l = l <= len ? l : len;
 
   strncpy(buf, buffer, l);
+  *(char *)(buf + l) = -1;
   // Log("events_read: %d\n", l);
   return l;
 }
