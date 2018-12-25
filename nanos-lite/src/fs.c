7a1394645507ca74782f617c87c6554e6ffc7204
tracer-ics2018 <tracer@njuics.org>
2018-12-25 20:29:54 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:29:54 up  8:24,  1 user,  load average: 0.40, 0.51, 0.65 9f0e48fad0ef543df04bef71c5439186eaf5ca2f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -65,7 +65,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 ssize_t fs_read(int fd, void *buf, size_t len) {
   // assert(file_table[fd].open_offset + len <= file_table[fd].size);
   size_t l = file_table[fd].open_offset + len <= file_table[fd].size ? len : file_table[fd].size - file_table[fd].open_offset;
-  if (file_table[fd].read != NULL) ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
+  if (file_table[fd].read == NULL) ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   else file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   // Log("fs_read: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
@@ -76,7 +76,7 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
   // assert(file_table[fd].open_offset + len <= file_table[fd].size);
   size_t l = file_table[fd].open_offset + len <= file_table[fd].size ? len : file_table[fd].size - file_table[fd].open_offset;
   // ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
-  if (file_table[fd].write != NULL) ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
+  if (file_table[fd].write == NULL) ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   else file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, l);
   file_table[fd].open_offset += l;
   // Log("fs_write: %s offset: 0x%x length: 0x%x\n", file_table[fd].name, file_table[fd].open_offset, l);
