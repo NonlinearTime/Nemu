559f3518ca0eec5aa616296d525dcf1cf29b3bb0
tracer-ics2018 <tracer@njuics.org>
2018-12-25 23:44:03 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:44:03 up 11:38,  1 user,  load average: 1.57, 1.36, 1.00 5b6d1e8bbf25d2bc6d67b0e8d1e816358f10345c

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -27,7 +27,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stderr", 0, 0, 0, invalid_read, serial_write},
   {"/dev/fb",0, 0, 0, invalid_read, fb_write},
   {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
-  {"/dev/events", 0x7fffffff, 0, 0, events_read, invalid_write}, 
+  {"/dev/events", 0, 0, 0, events_read, invalid_write}, 
   {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
