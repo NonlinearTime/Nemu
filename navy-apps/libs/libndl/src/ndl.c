e06d0774e1615e57454e6bbbd86c4a8e42e1bbe7
tracer-ics2018 <tracer@njuics.org>
2018-12-25 10:04:33 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:04:33 up  1:18,  1 user,  load average: 0.32, 0.39, 0.44 b9f95f885d6995e88b77afe7ea7a4422e871a34

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -138,6 +138,7 @@ static void get_display_info() {
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
   fclose(dispinfo);
+  printf("get_display_info: w %d h %d\n", screen_w, screen_h);
   assert(screen_w > 0 && screen_h > 0);
 }
 
