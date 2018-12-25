2745cf445f35ce96cc23df5cecbc27ff767ab9a2
tracer-ics2018 <tracer@njuics.org>
2018-12-25 12:17:04 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:17:04 up 11 min,  1 user,  load average: 0.29, 0.61, 0.56 7d5617912871780491a61ebfaa665a9e92badee3

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,7 +24,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   _Device *timmer = _device(1);
   _Device *input = _device(2);
   printf("events_read: %p %p\n", timmer, input);
-  assert(0);
+  // assert(0);
   timmer->read(_DEVREG_TIMER_UPTIME, &uptime, 4);
   input->read(_DEV_INPUT, &kbd, 4);
   Log("events_read: %d\n", len);
