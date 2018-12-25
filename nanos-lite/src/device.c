0d71ebba5f1bb0d8e9fc69358d8bca6813774fc3
tracer-ics2018 <tracer@njuics.org>
2018-12-25 11:30:57 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:30:57 up  2:44,  1 user,  load average: 0.81, 0.70, 0.70 16982ab1b02f0e6ee37554bb1e027b10d51c2ef

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,7 +19,28 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  _KbdReg kbd;
+  _UptimeReg uptime;
+  _Device *timmer = _device(_DEV_TIMER);
+  _Device *input = _device(_DEV_INPUT);
+  timmer->read(_DEVREG_TIMER_UPTIME, &uptime, 4);
+  input->read(_DEV_INPUT, &kbd, 4);
+  char buffer[128];
+  int l;
+  if (kbd.keycode == _KEY_NONE) {
+    uint64_t hi = uptime.hi;
+    uint64_t lo = uptime.lo;
+    l = sprintf(buffer, "t %d\n", hi << 32 | lo);
+  } else {
+    if (kbd.keydown) l = sprintf(buffer, "kd %s\n", keyname[kbd.keycode]);
+    else l = sprintf(buffer, "ku %s\n", keyname[kbd.keycode]);
+  }
+  l = l <= len ? l : len;
+  int i;
+  for (i = 0; i < len; ++i) {
+    *(char *)buf++ = buffer[i];
+  }
+  return l;
 }
 
 static char dispinfo[128] __attribute__((used));
