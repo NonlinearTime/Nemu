f03aa76e3d38b3612f25fcdd220f6564f01e86df
tracer-ics2018 <tracer@njuics.org>
2018-12-25 20:36:36 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:36:36 up  8:31,  1 user,  load average: 1.16, 0.85, 0.74 b0aa847f7f524f2ea934079845f3f0fb3b1915

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -17,6 +17,8 @@ extern size_t fb_write(const void *buf, size_t offset, size_t len);
 extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
 extern size_t events_read(void *buf, size_t offset, size_t len);
 
+size_t evnt_read(void *buf, size_t offset, size_t len);
+
 /* This is the information about all files in disk. */
 
 static Finfo file_table[] __attribute__((used)) = {
@@ -25,7 +27,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stderr", 0, 0, invalid_read, serial_write},
   {"/dev/fb",0, 0, invalid_read, fb_write},
   {"/proc/dispinfo", 128, 0, dispinfo_read, invalid_write},
-  {"/dev/events", 128, 0, events_read, invalid_write}, 
+  {"/dev/events", 128, 0, evnt_read, invalid_write}, 
   {"/dev/tty", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
