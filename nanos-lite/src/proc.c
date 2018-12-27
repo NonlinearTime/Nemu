1d02199a66f75d9126ca6246015bd264c6db56b4
tracer-ics2018 <tracer@njuics.org>
2018-12-27 19:36:56 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:36:56 up 11:27,  1 user,  load average: 0.12, 0.24, 0.32 50cde604329918eb19bc450ae0bc39ab34eb5382

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,9 +32,9 @@ void init_proc() {
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  // current = &pcb[0];
+  current = &pcb[0];
 
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 
   return current->cp;
 }
