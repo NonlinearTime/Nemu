2eb2c4621026a9fa883b4c0042aa0dea30165864
tracer-ics2018 <tracer@njuics.org>
2018-12-27 19:18:57 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:18:57 up 11:09,  1 user,  load average: 0.14, 0.37, 0.39 e6b309decb5913c21dddfc5c3788d7dbfb15f0b6

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,7 +14,7 @@ extern size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
-  uint32_t len = fs_filesz(fd);
+  int len = fs_filesz(fd);
   uint32_t blen = pcb->as.pgsize;
   
   // uintptr_t s = DEFAULT_ENTRY;
@@ -23,6 +23,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   char buf[blen];
   while (len > 0) {
     void* page_base = new_page(1);
+    printf("loader: page_base %p\n", page_base);
     _map(&pcb->as, NULL, page_base, 0);
     fs_read(fd, buf, blen);
     memcpy(page_base, buf , blen);
