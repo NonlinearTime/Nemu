262aea76ee9d2a460dbf0fb1804a899d7ce55cf7
tracer-ics2018 <tracer@njuics.org>
2018-12-28 00:32:25 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  00:32:25 up 16:22,  1 user,  load average: 0.53, 0.61, 0.72 8cd6c6d04f64f03a2e770b38700a3ecb11e819c8

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -27,6 +27,7 @@ int mm_brk(uintptr_t new_brk) {
     if (_map(&current->as, (void *)vaddr_start, NULL, MAP_TEST)) vaddr_start += current->as.pgsize;
     while (vaddr_start < new_brk) {
       void* page_base = new_page(1);
+      Log("mm_brk: vaddr_start 0x%x new_brk 0x%x\n", vaddr_start, new_brk);
       _map(&current->as, (void *)vaddr_start, page_base, MAP_CREATE);
       vaddr_start += current->as.pgsize;
     }
