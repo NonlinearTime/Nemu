24613186e1f13b595c472b880ffb67b15da36dfc
tracer-ics2018 <tracer@njuics.org>
2018-12-28 11:20:56 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:20:56 up  3:19,  1 user,  load average: 0.53, 0.42, 0.30 fd6a1837c15393d0e63361de2c2efcd0d28aaa1e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,6 +34,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
       l = sprintf(buf, "kd %s\n", keyname[kc & 0xfff]);
       if ((kc & 0xfff) == _KEY_F1) {
         fg_pcb = pcbs[1];
+        Log("F1: %p", fg_pcb);
       } else if ((kc & 0xfff) == _KEY_F2) {
         fg_pcb = pcbs[2];
       }
