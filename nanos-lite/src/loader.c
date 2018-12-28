01bfab9f5f2ff7d5b6045c6dc6157c90df1a81a2
tracer-ics2018 <tracer@njuics.org>
2018-12-28 08:44:33 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  08:44:33 up 42 min,  1 user,  load average: 0.64, 0.32, 0.26 f380012888d26391dcccfc0bdf8959ef147c446

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,6 +34,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     len -= blen;
   }
   pcb->cur_brk = s;
+  Log("loader: cur_brk %p s: %p\n", pcb->cur_brk, s);
   fs_close(fd);
 
   // Log("file length: 0x%x\n", len);
