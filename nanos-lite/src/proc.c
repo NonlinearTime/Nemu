7582b0c9e6a04cbfa30a7285b1e7b000ac5d3d73
tracer-ics2018 <tracer@njuics.org>
2018-12-28 08:50:37 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  08:50:37 up 48 min,  1 user,  load average: 0.67, 0.65, 0.43 c588e9d8a052eeb5ebfa37e1bbde4dd1383c38e

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   // naive_uload(&pcb_boot, "/bin/init");
   // context_kload(&pcb[0], (void *)hello_fun);
-  context_kload(&pcb[0], "/bin/hello");
+  context_uload(&pcb[0], "/bin/hello");
   context_uload(&pcb[1], "/bin/pal");
   pcb_boot = pcb[1];
   switch_boot_pcb();
