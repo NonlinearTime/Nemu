93b1349e19a87e3036687a3b71ec731ae132dd8f
tracer-ics2018 <tracer@njuics.org>
2018-12-28 09:55:46 +0800

    >  run U201514716 haines Linux haines-pc 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:55:46 up  1:53,  1 user,  load average: 0.37, 0.33, 0.31 4d2286ac7bc731210ac30c887d052ef3593e64a

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -61,7 +61,7 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
   }
 
   // -------------------- system call --------------------------
-  idt[0x32] = GATE(STS_TG32, KSEL(SEG_KCODE), irq0, DPL_KERN);
+  idt[32] = GATE(STS_TG32, KSEL(SEG_KCODE), irq0, DPL_KERN);
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_KERN);
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
 
